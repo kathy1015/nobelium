59bccc7fa0039a4a445eb3c73f54e557c9ef6b3d
kathy1015 <89855813+kathy1015@users.noreply.github.com>
2021-08-31 22:34:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'Kathy's pixel art',
+  author: 'Kathy Chow,
+  email: 'kathy1015@qq.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'Welcom to my pixel gallery',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
